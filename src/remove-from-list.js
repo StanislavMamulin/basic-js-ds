af43fd88a720c64b138e3ce6a9331595344e5d88
Stanislav Mamulin <StanislavPM@ya.ru>
2022-10-16 17:55:36 +0500

    feat: add solution to "Remove from list" task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,7 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
-
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -22,9 +18,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let currentNode = l;
+  let prevNode = null;
+  let firstNode = l;
+
+  while (currentNode) {
+    if (currentNode.value === k) {
+      if (prevNode) {
+        prevNode.next = currentNode.next;
+      } else {
+        firstNode = currentNode.next;
+      }
+    } else {
+      prevNode = currentNode;
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  return firstNode;
 }
 
 module.exports = {
